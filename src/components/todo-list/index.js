c55fe12fb868233c81fa1954f61ccb05ce507080
KojoFocus <ernestobimpeh9@gmail.com>
2024-02-15 12:10:51 +0000

    my todo app

---
+++ src/components/todo-list/index.js
@@ -0,0 +1,10 @@
+function TodoList() {
+    return (
+        <ul>
+            <li>Complete online Javascript Course</li>
+            <li>Jog around the park three times</li>
+        </ul>
+    )
+}
+
+export default TodoList;
